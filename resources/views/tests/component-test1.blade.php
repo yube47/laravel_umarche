bd3a0bf2af2adae3f4938bd3551b60561d7b210e
yube47 <myymbe0125@gmail.com>
2023-02-12 15:33:16 +0900

    変数@

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,10 @@
+
 <x-tests.app>
+    <x-slot name="header">
+        ヘッダー１
+    </x-slot>
+
 コンポーネントテスト1
+    <x-tests.card title="タイトル1" content="本文1" :message="$message"></x-tests.app>
 </x-tests.app>
 
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,10 @@
+
 <x-tests.app>
+<x-slot name="header">
+ヘッダー２
+</x-slot>
 コンポーネントテスト2
+
+<x-tests.card title="タイトル2" content="本文2"></x-tests.app>
+
 </x-tests.app>
